2b2fcc1282ce8a9da77697cb7c2191184dae32c2
jota <jotica@gmail.com>
2019-01-21 22:40:06 +0000

    Add display of avg of bio data

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -13,6 +13,18 @@ class Workout < ApplicationRecord
     bio_entries.last.heart_rate
   end
 
+  def avg_hr
+    return 0 if bio_entries.empty?
+    values = bio_entries.pluck(:heart_rate)
+    values.inject(:+) / values.count 
+  end
+
+  def avg_temp
+    return 0 if bio_entries.empty?
+    values = bio_entries.pluck(:temperature)
+    values.inject(:+) / values.count 
+  end
+
   def duration 
     return 0 if reps.empty?
     reps.last.date - reps.first.date
